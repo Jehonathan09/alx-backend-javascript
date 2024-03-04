86dda0313a825f2c86c0768435bad361a3da8d01
jehonathan09 <jonathanmwenge64@@gmail.com>
2024-03-04 20:38:57 +0000

    all tasks

---
+++ 0-constants.js
@@ -0,0 +1,17 @@
+export function taskFirst(){
+	const task = 'I prefer const whenI can.';
+	return task;
+}
+
+export function getlast(){
+	return ' is okay';
+}
+
+export function taskNext(){
+	let combination = 'But sometimes let';
+	combination += getLast();
+
+	return combination;
+}
+
+
